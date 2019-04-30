9ab0843aec194ce6bfab1b0576df0e7f25380c8b
ice417cream <1210494158@qq.com>
2019-04-30 15:44:04 +0800

    全局变量和局部训练器设计

--- trainer/A3C_trainer.py
+++ trainer/A3C_trainer.py
@@ -4,27 +4,27 @@ import gym
 from trainer.trainer_base import Agent_trainer
 
 class A3C_trainer(Agent_trainer):
-    def __init__(self,name, env, scope):
-        print("mtmadan trianer init")
-        if scope == GLOBAL_NET_SCOPE:   # get global network
+    def __init__(self, scope,N_S,N_A,globalAC = None):
+        print("A3C_trianer init")
+        if scope == 'Global_Net':   # get global network
+            print('global init')
             with tf.variable_scope(scope):
                 self.s = tf.placeholder(tf.float32, [None, N_S], 'S')
-                self.a_params, self.c_params = self.build_net(scope)[-2:]
+                self.a_params, self.c_params = self.build_net(scope,N_A)[-2:]
+                print('get global params')
         else:   # local net, calculate losses
             with tf.variable_scope(scope):
                 self.s = tf.placeholder(tf.float32, [None, N_S], 'S')
                 self.a_his = tf.placeholder(tf.float32, [None, N_A], 'A')
                 self.v_target = tf.placeholder(tf.float32, [None, 1], 'Vtarget')
 
-                mu, sigma, self.v, self.a_params, self.c_params = self.build_net(scope)
+                mu, sigma, self.v, self.a_params, self.c_params = self.build_net(scope,N_A)
 
                 td = tf.subtract(self.v_target, self.v, name='TD_error')
                 with tf.name_scope('c_loss'):
                     self.c_loss = tf.reduce_mean(tf.square(td))
-
-                with tf.name_scope('wrap_a_out'):
-                    mu, sigma = mu * A_BOUND[1], sigma + 1e-4
-
+                # with tf.name_scope('wrap_a_out'):
+                #     mu, sigma = mu * A_BOUND[1], sigma + 1e-4
                 normal_dist = tf.distributions.Normal(mu, sigma)
 
                 with tf.name_scope('a_loss'):
@@ -35,7 +35,7 @@ class A3C_trainer(Agent_trainer):
                     self.a_loss = tf.reduce_mean(-self.exp_v)
 
                 with tf.name_scope('choose_a'):  # use local params to choose action
-                    self.A = tf.clip_by_value(tf.squeeze(normal_dist.sample(1), axis=[0, 1]), A_BOUND[0], A_BOUND[1])
+                    self.A = tf.squeeze(normal_dist.sample(1), axis=[0, 1])
                 with tf.name_scope('local_grad'):
                     self.a_grads = tf.gradients(self.a_loss, self.a_params)
                     self.c_grads = tf.gradients(self.c_loss, self.c_params)
@@ -48,67 +48,7 @@ class A3C_trainer(Agent_trainer):
                     self.update_a_op = OPT_A.apply_gradients(zip(self.a_grads, globalAC.a_params))
                     self.update_c_op = OPT_C.apply_gradients(zip(self.c_grads, globalAC.c_params))
 
-
-
-    def train(self):
-        print("training")
-        global GLOBAL_RUNNING_R, GLOBAL_EP, SESS
-        total_step = 1
-        buffer_s, buffer_a, buffer_r = [], [], []
-        while not self.coord.should_stop() and GLOBAL_EP < 2000:
-            s = self.env.reset()  # 列表
-            ep_r = 0
-            for ep_t in range(200):
-                # if self.name == 'W_0':
-                #     self.env.render()
-                a = self.AC.choose_action(s)
-                s_, r, done, info = self.env.step(a)  # 列表
-                done = True if ep_t == 200 - 1 else False
-
-                ep_r += r
-                buffer_s.append(s)
-                buffer_a.append(a)
-                buffer_r.append((r + 8) / 8)  # normalize
-
-                if total_step % 10 == 0 or done:  # update global and assign to local net
-                    if done:
-                        v_s_ = 0  # terminal
-                    else:
-                        v_s_ = SESS.run(self.AC.v, {self.AC.s: s_[np.newaxis, :]})[0, 0]
-                    buffer_v_target = []
-                    for r in buffer_r[::-1]:  # reverse buffer r
-                        v_s_ = r + 0.9 * v_s_
-                        buffer_v_target.append(v_s_)
-                    buffer_v_target.reverse()
-
-                    buffer_s, buffer_a, buffer_v_target = np.vstack(buffer_s), np.vstack(buffer_a), np.vstack(
-                        buffer_v_target)
-                    feed_dict = {
-                        self.AC.s: buffer_s,
-                        self.AC.a_his: buffer_a,
-                        self.AC.v_target: buffer_v_target,
-                    }
-                    self.AC.update_global(feed_dict)
-                    buffer_s, buffer_a, buffer_r = [], [], []
-                    self.AC.pull_global()
-
-                s = s_
-                total_step += 1
-                if done:
-                    if len(GLOBAL_RUNNING_R) == 0:  # record running episode reward
-                        GLOBAL_RUNNING_R.append(ep_r)
-                    else:
-                        GLOBAL_RUNNING_R.append(0.9 * GLOBAL_RUNNING_R[-1] + 0.1 * ep_r)
-                    print(
-                        self.name,
-                        "Ep:", GLOBAL_EP,
-                        "| Ep_r: %i" % GLOBAL_RUNNING_R[-1],
-                        "golbal_len %d" % len(GLOBAL_RUNNING_R)
-                    )
-                    GLOBAL_EP += 1
-                    break
-
-    def build_net(self,scope):
+    def build_net(self,scope,N_A):
         print("building net")
         w_init = tf.random_normal_initializer(0., .1)
         # 建立actor的网络
@@ -116,7 +56,6 @@ class A3C_trainer(Agent_trainer):
             l_a = tf.layers.dense(self.s, 200, tf.nn.relu6, kernel_initializer=w_init, name='la')
             mu = tf.layers.dense(l_a, N_A, tf.nn.tanh, kernel_initializer=w_init, name='mu')
             sigma = tf.layers.dense(l_a, N_A, tf.nn.softplus, kernel_initializer=w_init, name='sigma')
-
         # 建立critic的网络
         with tf.variable_scope('critic'):
             l_c = tf.layers.dense(self.s, 100, tf.nn.relu6, kernel_initializer=w_init, name='lc')
