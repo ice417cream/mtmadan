561c0b1f85b980a64da3a27c4c00be1f1f190e4f
wangziquan <bit505-02@lewis-z270>
2019-06-04 08:02:04 +0800

    add train_A3C

---
+++ train_A3C.py
@@ -0,0 +1,151 @@
+#lewis @bit
+import numpy as np
+import time
+import tensorflow as tf
+import trainer.A3C_trainer as T
+
+OUTPUT_GRAPH = True
+LOG_DIR = './log'
+N_WORKERS = 4
+MAX_EP_STEP = 2000
+MAX_GLOBAL_EP = 20000
+GLOBAL_NET_SCOPE = 'Global_Net'
+UPDATE_GLOBAL_ITER = 10
+GAMMA = 0.9
+ENTROPY_BETA = 0.01
+LR_A = 0.0001    # learning rate for actor
+LR_C = 0.001    # learning rate for critic
+GLOBAL_RUNNING_R = []
+GLOBAL_EP = 0
+agent_num = 10
+landmark_num = 1
+
+#[stop, right, left, up, down]
+action_dict = {"0": [0., 0., 1., 1., 0.], # l u
+               "1": [0., 0., 0., 1., 0.], #   u
+               "2": [0., 1., 0., 1., 0.], # r u
+               "3": [0., 1., 0., 0., 0.], # r
+               "4": [0., 1., 0., 0., 1.], # r d
+               "5": [0., 0., 0., 0., 1.], #   d
+               "6": [0., 0., 1., 0., 1.], # l d
+               "7": [0., 0., 1., 0., 0.], # l
+               "8": [1., 0., 0., 0., 0.]} #stop
+
+#creat the world
+def make_env(scenario):
+
+    print("make_env")
+    from MAEnv.environment import MultiAgentEnv
+    import MAEnv.scenarios as scenarios
+
+    scenario = scenarios.load(scenario+".py").Scenario()#建立一个类
+    world = scenario.make_World(agent_num, landmark_num)
+    env = MultiAgentEnv(world,scenario.reset_world,scenario.reward,scenario.observation)
+
+    return env,world
+
+class Worker(object):
+    def __init__(self, name, env, globalAC):
+        self.env = env
+        self.name = name
+        self.AC = T.ACNet(name, globalAC, OPT_A, OPT_C, SESS)
+
+    def work(self):
+        global GLOBAL_RUNNING_R, GLOBAL_EP
+        total_step = 1
+        buffer_s, buffer_a, buffer_r = [], [], []
+        while not COORD.should_stop() and GLOBAL_EP < MAX_GLOBAL_EP:
+            s = self.env.reset()
+            ep_r = 0
+            for ep_t in range(MAX_EP_STEP):
+                start = time.time()
+                a = self.AC.choose_action(s)
+                end = time.time()
+                #print("time:", end-start)
+                s_, r, done, info = self.env.step(a)
+                #print("s_ shape is %d" % s_.shape[0])
+                done = True if ep_t == MAX_EP_STEP - 1 else False
+
+                ep_r += r
+                buffer_s.append(s)
+                buffer_a.append(a)
+                buffer_r.append((r+8)/8)  # normalize
+
+                if total_step % UPDATE_GLOBAL_ITER == 0 or done:   # update global and assign to local net
+                    if done:
+                        v_s_ = 0   # terminal
+                    else:
+                        v_s_ = SESS.run(self.AC.v, {self.AC.s: s_[np.newaxis, :]})[0, 0]
+                    buffer_v_target = []
+                    for r in buffer_r[::-1]:    # reverse buffer r
+                        v_s_ = r + GAMMA * v_s_
+                        buffer_v_target.append(v_s_)
+                    buffer_v_target.reverse()
+
+                    buffer_s, buffer_a, buffer_v_target = np.vstack(buffer_s), np.vstack(buffer_a), np.vstack(buffer_v_target)
+                    feed_dict = {
+                        self.AC.s: buffer_s,
+                        self.AC.a_his: buffer_a,
+                        self.AC.v_target: buffer_v_target,
+                    }
+                    print(ep_t, " | c_loss", SESS.run(self.AC.c_loss, feed_dict=feed_dict))
+                    self.AC.update_global(feed_dict)
+                    buffer_s, buffer_a, buffer_r = [], [], []
+                   # self.AC.pull_global()
+
+                s = s_
+                total_step += 1
+                if done:
+                    if len(GLOBAL_RUNNING_R) == 0:  # record running episode reward
+                        GLOBAL_RUNNING_R.append(ep_r)
+                    else:
+                        GLOBAL_RUNNING_R.append(0.9 * GLOBAL_RUNNING_R[-1] + 0.1 * ep_r)
+                    # print(
+                    #     self.name,
+                    #     "Ep:", GLOBAL_EP,
+                    #     "| Ep_r: %i" % GLOBAL_RUNNING_R[-1],
+                    #     "golbal_len %d" % len(GLOBAL_RUNNING_R)
+                    #       )
+                    GLOBAL_EP += 1
+                    break
+
+
+if __name__ == "__main__":
+
+    SESS = tf.Session()
+    env, world = make_env("trainer_1_test")
+    with tf.device("/cpu:0"):
+        OPT_A = tf.train.RMSPropOptimizer(LR_A, name='RMSPropA')
+        OPT_C = tf.train.RMSPropOptimizer(LR_C, name='RMSPropC')
+        GLOBAL_AC = T.ACNet(GLOBAL_NET_SCOPE, env, OPT_A, OPT_C, SESS)  # we only need its params
+        workers = []
+        # Create worker
+        for i in range(N_WORKERS):
+            i_name = 'W_%i' % i   # worker name
+            workers.append(Worker(i_name, env, GLOBAL_NET_SCOPE))
+
+    #加入线程协调器
+    COORD = tf.train.Coordinator()
+
+    #初始化所有变量
+    SESS.run(tf.global_variables_initializer())
+
+    if OUTPUT_GRAPH:
+        if os.path.exists(LOG_DIR):
+            #递归删除文件夹
+            shutil.rmtree(LOG_DIR)
+        #将tf的图写入文件夹
+        tf.summary.FileWriter(LOG_DIR, SESS.graph)
+
+    worker_threads = []
+    for worker in workers:
+        job = lambda: worker.work()
+        t = threading.Thread(target=job)
+        t.start()
+        worker_threads.append(t)
+    COORD.join(worker_threads)
+
+    plt.plot(np.arange(len(GLOBAL_RUNNING_R)), GLOBAL_RUNNING_R)
+    plt.xlabel('step')
+    plt.ylabel('Total moving reward')
+    plt.show()
---
+++ trainer/A3C_trainer.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+import numpy as np
+import gym
+
+class ACNet(object):
+    def __init__(self, scope, env, OPT_A, OPT_C, SESS, globalAC=None):
+        self.env = env
+        obs_shape_n = [env.observation_space[i].shape for i in range(env.n)]
+        act_shape_n = [env.action_space[i] for i in range(env.n)]  # 返回值是离散空间Discrete
+        N_S = obs_shape_n[0][0]
+        self.N_A = 8
+        A_BOUND = [0,8]
+
+        self.SESS = SESS
+
+        if scope == "Global_net":   # get global network
+            with tf.variable_scope(scope):
+                self.s = tf.placeholder(tf.float32, [None, N_S], 'S')
+                self.a_params, self.c_params = self._build_net(scope)[-2:]
+        else:   # local net, calculate losses
+            with tf.variable_scope(scope):
+                self.s = tf.placeholder(tf.float32, [None, N_S], 'S')
+                self.a_his = tf.placeholder(tf.float32, [None, self.N_A], 'A')
+                self.v_target = tf.placeholder(tf.float32, [None, 1], 'Vtarget')
+
+                mu, sigma, self.v, self.a_params, self.c_params = self._build_net(scope)
+
+                self.td = tf.subtract(self.v_target, self.v, name='TD_error')
+                with tf.name_scope('c_loss'):
+                    self.c_loss = tf.reduce_mean(tf.square(self.td))
+
+                with tf.name_scope('wrap_a_out'):
+                    mu, sigma = mu * A_BOUND[1], sigma + 1e-4
+
+                normal_dist = tf.distributions.Normal(mu, sigma)
+
+                with tf.name_scope('a_loss'):
+                    log_prob = normal_dist.log_prob(self.a_his)
+                    exp_v = log_prob * tf.stop_gradient(self.td)
+                    entropy = normal_dist.entropy()  # encourage exploration
+                    self.exp_v = 0.01 * entropy + exp_v
+                    self.a_loss = tf.reduce_mean(-self.exp_v)
+
+                with tf.name_scope('choose_a'):  # use local params to choose action
+                    self.A = tf.clip_by_value(tf.squeeze(normal_dist.sample(1), axis=[0, 1]), A_BOUND[0], A_BOUND[1])
+
+                with tf.name_scope('local_grad'):
+                    self.a_grads = tf.gradients(self.a_loss, self.a_params)
+                    self.c_grads = tf.gradients(self.c_loss, self.c_params)
+
+            with tf.name_scope('sync'):
+                with tf.name_scope('pull'):
+                    self.pull_a_params_op = [l_p.assign(g_p) for l_p, g_p in zip(self.a_params, globalAC.a_params)]
+                    self.pull_c_params_op = [l_p.assign(g_p) for l_p, g_p in zip(self.c_params, globalAC.c_params)]
+                with tf.name_scope('push'):
+                    self.update_a_op = OPT_A.apply_gradients(zip(self.a_grads, globalAC.a_params))
+                    self.update_c_op = OPT_C.apply_gradients(zip(self.c_grads, globalAC.c_params))
+    #构建神经网络
+    def _build_net(self, scope):
+        w_init = tf.random_normal_initializer(0., .1)
+        #建立actor的网络
+        with tf.variable_scope('actor'):
+            l_a = tf.layers.dense(self.s, 200, tf.nn.relu6, kernel_initializer=w_init, name='la')
+            mu = tf.layers.dense(l_a, self.N_A, tf.nn.tanh, kernel_initializer=w_init, name='mu')
+            sigma = tf.layers.dense(l_a, self.N_A, tf.nn.softplus, kernel_initializer=w_init, name='sigma')
+
+        #建立critic的网络
+        with tf.variable_scope('critic'):
+            l_c = tf.layers.dense(self.s, 100, tf.nn.relu6, kernel_initializer=w_init, name='lc')
+            v = tf.layers.dense(l_c, 1, kernel_initializer=w_init, name='v')  # state value
+        a_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/actor')
+        c_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/critic')
+        return mu, sigma, v, a_params, c_params
+
+    #更新全局参数
+    def update_global(self, feed_dict):  # run by a local
+        self.SESS.run([self.update_a_op, self.update_c_op], feed_dict)  # local grads applies to global net
+
+    #拉取全局参数
+    def pull_global(self):  # run by a local
+        self.SESS.run([self.pull_a_params_op, self.pull_c_params_op])
+
+    #选择动作
+    def choose_action(self, s):  # run by a local
+        s = s[np.newaxis, :]
+        return self.SESS.run(self.A, {self.s: s})
